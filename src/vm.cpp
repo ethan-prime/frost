23279a9e9e9e75917512f946e1df11e052a962d3
ethan-prime <emh419@hotmail.com>
2025-12-11 16:36:40 -0600

    initial

---
+++ src/vm.cpp
@@ -0,0 +1,73 @@
+#include <cstdint>
+#include <array>
+
+constexpr std::size_t MEMORY_MAX = 1ul << 16;
+std::array<std::uint32_t, MEMORY_MAX> memory{};
+
+enum class Reg : std::uint8_t {
+    R0 = 0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    
+    PC, // program counter
+    SP, // stack pointer
+    FP, // frame pointer
+    FLAGS,
+
+    COUNT
+};
+
+struct RegFile {
+    static constexpr std::size_t count = static_cast<std::size_t>(Reg::COUNT);
+    std::array<std::uint32_t, count> registers{};
+
+    std::uint32_t& operator[](Reg r) {
+        return registers[static_cast<std::size_t>(r)];
+    }
+
+    const std::uint32_t& operator[](Reg r) const {
+        return registers[static_cast<std::size_t>(r)];
+    }
+};
+
+enum class Opcode : std::uint8_t {
+    BR,
+    ADD,
+    LD,
+    ST,
+    JSR, // jump register
+    AND,
+    LDR, // load register
+    STR, // store register
+    NOP, // no-op
+    NOT,
+    LDI, // load indirect
+    STI, // store indirect
+    JMP,
+    RES, // unused
+    LEA,
+    TRAP,
+};
+
+enum class Flag : std::uint32_t {
+    P = 1u << 0, // positive
+    Z = 1u << 1, // zero
+    N = 1u << 2, // negative
+};
+
+int main(int argc, const char* argv[]) {
+    // parse args
+    
+    RegFile reg;
+
+    reg[Reg::FLAGS] = static_cast<std::uint32_t>(Flag::Z);
+    reg[Reg::PC] = 0x3000;
+
+    
+}
+
